3da3d5c8a9ed471693ffc687fa44d8dcad1078e3
Emilio <emlem44@ulaval.ca>
2026-04-20 16:20:48 -0400

    adding video to ascii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from PIL import Image 
-from image_ascii_converter import convert_image_to_ascii, detect_edges, resize_image, get_difference_of_gaussian 
+from image_ascii_converter import convert_image_to_ascii, detect_edges, resize_image, get_difference_of_gaussian, convert_video_to_ascii
 from image_exporter import save_ascii_as_image, save_as_colored_html
 import numpy as np
 
@@ -11,10 +11,17 @@ if __name__ == "__main__":
     filename = "mario"
     image_path = f"{base_dir}/{filename}.png"
     output_path = f"{base_dir}/results/{filename}_ascii.png"
-    image = Image.open(image_path)
+    # image = Image.open(image_path)
 
-    ascii_image, color_image = convert_image_to_ascii(image)
+    # ascii_image, color_image = convert_image_to_ascii(image)
 
-    save_ascii_as_image(ascii_image, color_image)
-    save_as_colored_html(ascii_image, color_image)
+    # save_ascii_as_image(ascii_image, color_image)
+    # save_as_colored_html(ascii_image, color_image)
+    base_video_dir = "./video"
+    filename = "mario_clip"
+    video_path = f"{base_video_dir}/{filename}.mp4"
+    output_path = f"{base_video_dir}/results/{filename}_ascii.mp4"
+
+    convert_video_to_ascii(video_path=video_path, output_path=output_path)
+
 
