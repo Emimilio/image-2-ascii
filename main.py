9ba7c10a8f38310a4ee0b83701940da103463124
Emilio <emlem44@ulaval.ca>
2026-04-19 23:41:27 -0400

    full workflow

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 
 from PIL import Image 
-from image_ascii_converter import convert_image_to_ascii, save_ascii_as_image
+from image_ascii_converter import convert_image_to_ascii, get_image_high_freq, detect_edges, resize_image
+from image_exporter import save_ascii_as_image, save_as_colored_html
+import numpy as np
 
 
 if __name__ == "__main__":
 
     base_dir = "./images"
-    filename = "meme"
-    image_path = f"{base_dir}/{filename}.jpg"
+    filename = "flat_iron"
+    image_path = f"{base_dir}/{filename}.png"
     output_path = f"{base_dir}/results/{filename}_ascii.png"
     image = Image.open(image_path)
+    image = resize_image(image)
     ascii_matrix, color_matrix = convert_image_to_ascii(image)
-    save_ascii_as_image(ascii_matrix, color_matrix, output_path)
+
+    image = get_image_high_freq(image)
+
+    edge_matrix = detect_edges(image)
+
+    final_image = np.where(edge_matrix == " ", ascii_matrix, edge_matrix)
+
+    save_as_colored_html(final_image, color_matrix)
+    save_ascii_as_image(final_image, color_matrix, output_path)
+
