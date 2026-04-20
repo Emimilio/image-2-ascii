f683ad9f4326d0110a3205e4564f2fd368c6ea3b
Emilio <emlem44@ulaval.ca>
2026-04-20 00:44:30 -0400

    stuff

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ import numpy as np
 if __name__ == "__main__":
 
     base_dir = "./images"
-    filename = "mario"
+    filename = "black_circle"
     image_path = f"{base_dir}/{filename}.png"
     output_path = f"{base_dir}/results/{filename}_ascii.png"
-    image = Image.open(image_path)
-    image = resize_image(image)
-    color_matrix = np.array(image)
-    ascii_matrix = convert_image_to_ascii(image)
+    image = Image.open(image_path).convert("RGB")
+    image_resize = resize_image(image)
+    color_matrix = np.array(image_resize)
+    ascii_matrix = convert_image_to_ascii(image_resize)
 
-    image = get_image_high_freq(image)
-
-    edge_matrix = detect_edges(image)
+    high_freq_image = get_image_high_freq(image)
+    high_freq_image_resize = resize_image(high_freq_image)
+    edge_matrix = detect_edges(high_freq_image_resize)
 
     final_image = np.where(edge_matrix == " ", ascii_matrix, edge_matrix)
 
     save_as_colored_html(final_image, color_matrix)
-    save_ascii_as_image(final_image, color_matrix, output_path)
+    save_ascii_as_image(final_image, color_matrix)
 
