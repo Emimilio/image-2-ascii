cd60489d09c978b5ad1d41bf2c58c184b9ad59e3
Emilio <emlem44@ulaval.ca>
2026-05-06 17:46:31 -0400

    adding arguments

--- image_ascii_converter.py
+++ image_ascii_converter.py
@@ -6,16 +6,18 @@ from skimage import feature
 import utils
 
 
-def convert_image_to_ascii(image: Image) -> tuple[npt.NDArray, npt.NDArray]:
+def convert_image_to_ascii(image: Image, new_width: int=200, use_edge_detection: bool=True) -> tuple[npt.NDArray, npt.NDArray]:
     image = image.convert("RGB")
-    image_resized = utils.resize_image(image)
+    image_resized = utils.resize_image(image, new_width)
     gray_image = utils.normalize_image(np.array(image_resized.convert("L")))
 
-    canny_edge_mask = feature.canny(gray_image, sigma=1)
-    edge_matrix = utils.detect_edges(gray_image)
-    edge_matrix = np.where(canny_edge_mask, edge_matrix, " ")
+    ascii_image = utils.map_pixel_to_ascii(image_resized)
 
-    ascii_matrix = utils.map_pixel_to_ascii(image_resized)
-    ascii_image = np.where(edge_matrix == " ", ascii_matrix, edge_matrix)
+    if use_edge_detection:
+        canny_edge_mask = feature.canny(gray_image, sigma=1)
+        edge_matrix = utils.detect_edges(gray_image)
+        edge_matrix = np.where(canny_edge_mask, edge_matrix, " ")
+
+        ascii_image = np.where(edge_matrix == " ", ascii_image, edge_matrix)
 
     return ascii_image, np.array(image_resized)
